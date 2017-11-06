f1163d30de18ce5fdbfbda22d100d1bfbab3cefb
Ken Huh <ken123777@gmail.com>
2017-11-06 15:14:21 +0900

    Update git.sh

--- git.sh
+++ git.sh
@@ -103,6 +103,7 @@ git log
 # 예전 커밋으로 돌아가기
 # OPTION은 여러가지가 있지만, 대표적으로 --soft, --mixed, --hard가 있습니다.
 # 목표점이 되는 커밋의 hash값은 git log를 통해 확인할 수 있습니다.
+# 참고: https://git-scm.com/docs/git-reset
 git reset OPTION COMMIT_HASH
 
 
@@ -110,4 +111,5 @@ git reset OPTION COMMIT_HASH
 # 모든 브랜치는 처음 생성된 커밋 지점을 기준으로 생성됩니다.
 # 브랜치 생성후, 처음 기준점이 되었던 지점을 업데이트할 수 있습니다.
 # OPTION은 너무 많은 것들이 가능하지만, 대표적으로 새로운 기준점으로 삼고 싶은 브랜치 이름을 넣을 수 있습니다.
+# 참고: https://git-scm.com/docs/git-rebase
 git rebase OPTION
